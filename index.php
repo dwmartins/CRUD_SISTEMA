51a70db2f48a7e191503cba40f638976fddc934f
Douglas Martins <douglas5422@outlook.com>
2022-12-15 11:49:12 -0300

    feat: validações da pagina de login

--- index.php
+++ index.php
@@ -13,7 +13,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
+<body class="position-relative">
+
+    <div class="validacoes position-absolute w-100 mt-4">
+        <!-- Alertas -->
+    </div>
    
     <section class="tela_logar d-flex align-items-center justify-content-center">
         <form class="form_logar flex-column  d-flex align-items-center" action="#" method="POST">
@@ -22,12 +26,12 @@
             <h6>ENTRAR</h6>
 
             <div class="d-flex align-items-center">
-                <input type="text" name="email" placeholder="Seu e-mail">
+                <input type="text" name="email" placeholder="Seu e-mail" id="email">
                 <i class="bi bi-person icon_user"></i>
             </div>
             
             <div class="d-flex aling-items-center">
-                <input type="password" name="senha" placeholder="Sua senha">
+                <input type="password" name="senha" placeholder="Sua senha" id="senha">
                 <i class="bi bi-lock icon_lock"></i>
             </div>
 
@@ -38,5 +42,6 @@
         </form>
     </section>
 
+    <script src="./assets/js/login.js"></script>
 </body>
 </html>
